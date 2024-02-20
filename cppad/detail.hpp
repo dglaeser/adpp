6e1048c84866427c5216d019a6ec4c666bdc56c6
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2024-02-20 23:50:52 +0100

    [detail][storage] ensure storing copies for rvalues

--- cppad/detail.hpp
+++ cppad/detail.hpp
@@ -12,20 +12,22 @@ namespace cppad::detail {
 
     template<typename T>
     class Storage {
+        using Stored = std::conditional_t<std::is_lvalue_reference_v<T>, T, std::remove_cvref_t<T>>;
+
     public:
         template<typename _T>
-            requires(std::convertible_to<_T, T>)
-        constexpr explicit Storage(_T&& value)
+            requires(std::convertible_to<_T, Stored>)
+        constexpr explicit Storage(_T&& value) noexcept
         : _value{std::forward<_T>(value)}
         {}
 
         template<typename Self> requires(!std::is_lvalue_reference_v<Self>)
-        constexpr T&& get(this Self&& self) {
+        constexpr T&& get(this Self&& self) noexcept {
             return std::move(self._value);
         }
 
         template<typename Self>
-        constexpr auto& get(this Self& self) {
+        constexpr auto& get(this Self& self) noexcept {
             if constexpr (std::is_const_v<Self>)
                 return std::as_const(self._value);
             else
@@ -33,7 +35,7 @@ namespace cppad::detail {
         }
 
     private:
-        T _value;
+        Stored _value;
     };
 
     template<typename T>
