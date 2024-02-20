8e493b8f793c46cad2979fe103f7207a1fab6490
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2024-02-20 08:07:12 +0100

    [type_trags] add empty expression

--- cppad/concepts.hpp
+++ cppad/concepts.hpp
@@ -10,26 +10,13 @@
 namespace cppad {
 namespace concepts {
 
-#ifndef DOXYGEN
-namespace detail {
-
-    struct MockExpression {
-        constexpr double value() const { return 1.0; }
-
-        template<typename T>
-        constexpr double partial(T&&) const { return 1.0; }
-    };
-
-}  // namespace detail
-#endif  // DOXYGEN
-
 template<typename T>
 concept Arithmetic = std::floating_point<std::remove_cvref_t<T>> or std::integral<std::remove_cvref_t<T>>;
 
 template<typename T>
 concept Expression = requires(const T& t) {
     { t.value() } -> Arithmetic;
-    { t.partial(detail::MockExpression{}) } -> Arithmetic;
+    { t.partial(EmptyExpression{}) } -> Arithmetic;
 };
 
 template<typename T>
--- cppad/type_traits.hpp
+++ cppad/type_traits.hpp
@@ -15,4 +15,15 @@ namespace detail {
 template<typename T>
 inline constexpr bool is_complete = !decltype(detail::is_incomplete(std::declval<T*>()))::value;
 
+struct EmptyExpression {
+    constexpr double value() const {
+        return 0.0;
+    }
+
+    template<typename T>
+    constexpr double partial(T&&) const {
+        return 0.0;
+    }
+};
+
 }  // namespace cppad
