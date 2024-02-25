a03b553bde60b739415a83c47afa3452edc53197
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2024-02-25 11:43:46 +0100

    [bw][deriv] pull out accessor

--- cppad/backward/derivative.hpp
+++ cppad/backward/derivative.hpp
@@ -1,70 +1,40 @@
 #pragma once
 
 #include <tuple>
+#include <array>
+#include <algorithm>
 #include <type_traits>
 
 #include <cppad/common.hpp>
+#include <cppad/variadic_accessor.hpp>
 
 namespace cppad::backward {
 
-#ifndef DOXYGEN
-namespace detail {
-
-template<concepts::arithmetic R, std::size_t I, typename T>
-struct derivative_element {
-    using index = index_constant<I>;
-
-    constexpr derivative_element(const T& t) : _t{t} {}
-
- protected:
-    constexpr auto& get(const index&) noexcept { return _value; }
-    constexpr const auto& get(const index&) const noexcept { return _value; }
-    constexpr auto get_index_of(const T&) const noexcept { return index{}; }
-    constexpr const T& get_ref(const index& = {}) const noexcept { return _t; }
-
- private:
-    const T& _t;
-    R _value = R{0};
-};
-
-template<typename... Ts>
-struct derivative_accessor;
-
-template<concepts::arithmetic R, std::size_t... I, typename... Ts>
-struct derivative_accessor<R, std::index_sequence<I...>, Ts...> : derivative_element<R, I, Ts>... {
-    constexpr derivative_accessor(const Ts&... ts)
-    : derivative_element<R, I, Ts>(ts)...
-    {}
-
- protected:
-    using derivative_element<R, I, Ts>::get_index_of...;
-    using derivative_element<R, I, Ts>::get...;
-};
-
-}  // namespace detail
-#endif  // DOXYGEN
-
 template<concepts::arithmetic R, typename... Ts>
     requires(are_unique_v<Ts...>)
-struct derivatives : detail::derivative_accessor<R, std::make_index_sequence<sizeof...(Ts)>, Ts...> {
+struct derivatives : variadic_accessor<Ts...> {
  private:
-    using base = detail::derivative_accessor<R, std::make_index_sequence<sizeof...(Ts)>, Ts...>;
+    using base = variadic_accessor<Ts...>;
 
  public:
     constexpr derivatives(R, const Ts&... ts) noexcept
-    : base(ts...)
-    {}
+    : base(ts...) {
+        std::ranges::fill(_values, R{0});
+    }
 
     template<typename T> requires(contains_decay_v<T, Ts...>)
     constexpr R operator[](const T& t) const noexcept {
-        return this->get(this->get_index_of(t));
+        return _values[this->index_of(t)];
     }
 
     template<typename T> requires(contains_decay_v<T, Ts...>)
     constexpr void add_to_derivative_wrt(const T& t, concepts::arithmetic auto value) noexcept {
         if constexpr (contains_decay_v<T, Ts...>)
-            this->get(this->get_index_of(t)) += value;
+            _values[this->index_of(t)] += value;
     }
+
+ private:
+    std::array<R, sizeof...(Ts)> _values;
 };
 
 template<typename R, typename... Ts>
---
+++ cppad/variadic_accessor.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <type_traits>
+
+#include <cppad/common.hpp>
+
+
+namespace cppad {
+
+#ifndef DOXYGEN
+namespace detail {
+
+template<std::size_t I, typename T>
+struct variadic_element {
+    using index = index_constant<I>;
+
+    constexpr variadic_element(const T& t) noexcept : _t{t} {}
+    constexpr const T& get(const index&) const noexcept { return _t; }
+    constexpr index index_of(const T&) const noexcept { return {}; }
+
+ private:
+    const T& _t;
+};
+
+template<typename... Ts>
+struct variadic_accessor;
+
+template<std::size_t... I, typename... Ts>
+struct variadic_accessor<std::index_sequence<I...>, Ts...> : variadic_element<I, Ts>... {
+    constexpr variadic_accessor(const Ts&... ts)
+    : variadic_element<I, Ts>(ts)...
+    {}
+
+    using variadic_element<I, Ts>::index_of...;
+    using variadic_element<I, Ts>::get...;
+};
+
+}  // namespace detail
+#endif  // DOXYGEN
+
+template<typename... Ts>
+    requires(are_unique_v<Ts...>)
+struct variadic_accessor : detail::variadic_accessor<std::make_index_sequence<sizeof...(Ts)>, Ts...> {
+ private:
+    using base = detail::variadic_accessor<std::make_index_sequence<sizeof...(Ts)>, Ts...>;
+
+ public:
+    constexpr variadic_accessor(const Ts&... ts) noexcept
+    : base(ts...)
+    {}
+
+    template<typename T> requires(contains_decay_v<T, Ts...>)
+    constexpr auto index(const T& t) const noexcept {
+        return this->get(this->index_of(t));
+    }
+};
+
+template<typename... Ts>
+    requires(std::conjunction_v<std::is_lvalue_reference<Ts>...>)
+variadic_accessor(Ts&&...) -> variadic_accessor<std::remove_cvref_t<Ts>...>;
+
+}  // namespace cppad
