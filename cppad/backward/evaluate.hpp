65206a794f4b567e8d8e1d33d3346fa434feb43c
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-02-27 21:03:00 +0100

    fixup ~

--- cppad/backward/evaluate.hpp
+++ cppad/backward/evaluate.hpp
@@ -33,12 +33,34 @@ struct expression {
         return _e.evaluate_at(bindings);
     }
 
+    operator const E&() const {
+        return _e;
+    }
+
  private:
     E _e;
 };
 
 template<typename E>
 expression(E&&) -> expression<std::remove_cvref_t<E>>;
+
+namespace traits {
+
+template<typename E>
+struct is_leaf_expression<expression<E>>
+: public is_leaf_expression<std::remove_cvref_t<E>> {};
+
+template<typename E>
+    requires(!traits::is_leaf_expression<E>::value)
+struct sub_expressions<expression<E>> {
+    static constexpr decltype(auto) get(const expression<E>& e) {
+        return sub_expressions<std::remove_cvref_t<E>>::get(
+            static_cast<const std::remove_cvref_t<E>&>(e)
+        );
+    }
+};
+
+}  // namespace traits
 
 template<typename E, typename... B>
     requires(detail::expression<E, B...>)
