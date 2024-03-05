ebb4542f2369b08e323a8589ee4313475470792f
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2024-03-05 08:08:18 +0100

    [benchmark] generalize script

--- benchmark/backwards/evaluate.py
+++ benchmark/backwards/evaluate.py
@@ -1,14 +1,46 @@
 import os
+import sys
 import subprocess
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-n", "--name",
+    required=True,
+    help="The name of the benchmark"
+)
+parser.add_argument(
+    "-r", "--reference",
+    required=False,
+    help="A reference implementation of the benchmark to compare against"
+)
+parser.add_argument(
+    "-a", "--args",
+    required=False,
+    help="Runtime arguments to be passed to benchmark invocations"
+)
+args = vars(parser.parse_args())
+
+name = args["name"]
+ref_name = args.get("reference")
 subprocess.run(["make", "clean"], check=True)
-subprocess.run(["time", "make", "backward_autodiff"], check=True)
-subprocess.run(["time", "make", "backward_adpp"], check=True)
+subprocess.run(["time", "make", name], check=True)
+if ref_name is not None:
+    subprocess.run(["time", "make", ref_name], check=True)
 
-autodiff_size = os.path.getsize("backward_autodiff")
-adpp_size = os.path.getsize("backward_adpp")
-print("autodiff binary size: {:.2f} MB".format(autodiff_size/1e6))
-print("    adpp binary size: {:.2f} MB".format(adpp_size/1e6))
-print("               ratio: {:.2f}".format(autodiff_size/adpp_size))
+adpp_size = os.path.getsize(name)
+print("     adpp binary size: {:.2f} MB".format(adpp_size/1e6))
+if ref_name is not None:
+    ref_size = os.path.getsize(f"{name}_autodiff")
+    print("reference binary size: {:.2f} MB".format(ref_size/1e6))
+    print("                ratio: {:.2f}".format(ref_size/adpp_size))
 
-subprocess.run(["hyperfine", "--warmup", "10", "./backward_autodiff 2.0 4.0", "./backward_adpp 2.0 4.0"], check=True)
+call_args = args.get("args") or ""
+if ref_name is not None:
+    subprocess.run([
+        "hyperfine", "--warmup", "10", f"./{ref_name} {call_args}", f"./{name} {call_args}"
+    ], check=True)
+else:
+    subprocess.run([
+        "hyperfine", "--warmup", "10", f"./{name} {call_args}"
+    ], check=True)
