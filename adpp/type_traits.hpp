780fa93bc03e5651496eda007108b346830a5fb5
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2024-03-05 07:47:35 +0100

    [type_traits] remove obsolete public keywords

--- adpp/type_traits.hpp
+++ adpp/type_traits.hpp
@@ -22,21 +22,21 @@ struct type_list {};
 
 
 template<typename T>
-struct is_ownable : public std::bool_constant<!std::is_lvalue_reference_v<T>> {};
+struct is_ownable : std::bool_constant<!std::is_lvalue_reference_v<T>> {};
 template<typename T>
 inline constexpr bool is_ownable_v = is_ownable<T>::value;
 
 
 template<typename T, typename... Ts>
-struct is_any_of : public std::bool_constant<std::disjunction_v<std::is_same<T, Ts>...>> {};
+struct is_any_of : std::bool_constant<std::disjunction_v<std::is_same<T, Ts>...>> {};
 template<typename T, typename... Ts>
-struct is_any_of<T, type_list<Ts...>> : public is_any_of<T, Ts...> {};
+struct is_any_of<T, type_list<Ts...>> : is_any_of<T, Ts...> {};
 template<typename T, typename... Ts>
 inline constexpr bool is_any_of_v = is_any_of<T, Ts...>::value;
 
 
 template<typename T, typename... Ts>
-struct contains_decay : public is_any_of<std::decay_t<T>, Ts...> {};
+struct contains_decay : is_any_of<std::decay_t<T>, Ts...> {};
 template<typename T, typename... Ts>
 inline constexpr bool contains_decay_v = contains_decay<T, Ts...>::value;
 
@@ -50,9 +50,9 @@ struct are_unique<T1, T2, Ts...> {
         are_unique<T1, Ts...>::value &&
         are_unique<T2, Ts...>::value;
 };
-template<typename T1, typename T2> struct are_unique<T1, T2> : public std::bool_constant<!std::is_same_v<T1, T2>> {};
-template<typename T> struct are_unique<T> : public std::true_type {};
-template<> struct are_unique<> : public std::true_type {};
+template<typename T1, typename T2> struct are_unique<T1, T2> : std::bool_constant<!std::is_same_v<T1, T2>> {};
+template<typename T> struct are_unique<T> : std::true_type {};
+template<> struct are_unique<> : std::true_type {};
 template<typename... Ts>
 inline constexpr bool are_unique_v = are_unique<Ts...>::value;
 
@@ -70,10 +70,10 @@ namespace detail {
     };
 
     template<typename T>
-    struct unique_tuple<T> : public std::type_identity<type_list<T>> {};
+    struct unique_tuple<T> : std::type_identity<type_list<T>> {};
 
     template<typename... Ts>
-    struct unique_tuple<type_list<Ts...>> : public std::type_identity<type_list<Ts...>> {};
+    struct unique_tuple<type_list<Ts...>> : std::type_identity<type_list<Ts...>> {};
 
     template<typename... Ts, typename T, typename... Rest>
     struct unique_tuple<type_list<Ts...>, T, Rest...> {
@@ -146,7 +146,7 @@ namespace detail {
 #endif  // DOXYGEN
 
 template<typename T>
-struct is_complete : public std::bool_constant<!decltype(detail::is_incomplete(std::declval<T*>()))::value> {};
+struct is_complete : std::bool_constant<!decltype(detail::is_incomplete(std::declval<T*>()))::value> {};
 template<typename T>
 inline constexpr bool is_complete_v = is_complete<T>::value;
 
