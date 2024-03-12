5db73fe1efae73daa7d4d6a7a86ba36b41401057
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-03-12 09:04:43 +0100

    [test][bw][symbols] test deduced value stored type

--- test/backward/test_symbols_operators.cpp
+++ test/backward/test_symbols_operators.cpp
@@ -1,17 +1,35 @@
 #include <cstdlib>
+#include <type_traits>
 
 #include <boost/ut.hpp>
 
 #include <adpp/backward/symbols.hpp>
 
-// TODO: This include should not be necessary
+// TODO: This include should not be necessary?
 #include <adpp/backward/expression.hpp>
 
+// TODO: simplify this? Required for "function"...
+#include <adpp/backward/evaluate.hpp>
+
 using boost::ut::operator""_test;
+using boost::ut::expect;
+using boost::ut::eq;
 
 using adpp::backward::var;
-using adpp::backward::let;
 using adpp::backward::val;
+using adpp::backward::value;
+using adpp::backward::function;
+
+
+template<typename T> struct is_value : std::false_type {};
+template<typename T, auto _> struct is_value<adpp::backward::value<T, _>> : std::true_type {};
+
+template<typename E>
+struct value_stored_type {
+    using _values = adpp::filtered_tuple_t<is_value, adpp::backward::symbols_t<E>>;
+    static_assert(adpp::type_size_v<_values> == 1);
+    using type = typename adpp::first_type_t<_values>::stored_type;
+};
 
 
 int main() {
@@ -30,6 +48,32 @@ int main() {
             [[maybe_unused]] auto op_right = a - val<1>;
             [[maybe_unused]] auto op_left = val<1> - a;
         }
+    };
+
+    "expr_operation_with_owned_value"_test = [] () {
+        var a;
+        function f = a*2.0;
+        static_assert(std::is_same_v<
+            typename value_stored_type<std::remove_cvref_t<decltype(f)>>::type,
+            double
+        >);
+        static_assert(std::is_same_v<decltype(f(a = 2)), double>);
+        expect(eq(f(a = 2), 4));
+    };
+
+    "expr_operation_with_referenced_value"_test = [] () {
+        double v = 2.0;
+        var a;
+        function f = a*v;
+        static_assert(std::is_same_v<
+            typename value_stored_type<std::remove_cvref_t<decltype(f)>>::type,
+            double*
+        >);
+        static_assert(std::is_same_v<decltype(f(a = 2)), double>);
+        expect(eq(f(a = 2), 4));
+
+        v = 8.0;
+        expect(eq(f(a = 2), 16));
     };
 
     return EXIT_SUCCESS;
