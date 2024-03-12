073fa7268d6c4f2776d07d204ff918934f678789
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-03-12 21:25:10 +0100

    [bw] clean-up convenience header

--- adpp/backward.hpp
+++ adpp/backward.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <adpp/backward/symbols.hpp>
+#include <adpp/backward/operators.hpp>
 #include <adpp/backward/evaluate.hpp>
 #include <adpp/backward/differentiate.hpp>
-#include <adpp/backward/stream.hpp>
+#include <adpp/backward/io.hpp>
