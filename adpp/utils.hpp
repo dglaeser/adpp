40034f21d96c556c716a33854d66f6faba7bf278
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2024-03-25 18:36:53 +0100

    introduce utils header

---
+++ adpp/utils.hpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2024 Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
+// SPDX-License-Identifier: MIT
+/*!
+ * \file
+ * \ingroup Utilities
+ * \brief Utility classes and concepts.
+ */
+
+#pragma once
+
+#include <type_traits>
+
+namespace adpp {
+
+//! \addtogroup Utilities
+//! \{
+
+/*!
+ * \brief Can be used to signal e.g. automatic type deduction.
+ */
+struct automatic {};
+
+//! \} group Common
+
+}  // namespace adpp
